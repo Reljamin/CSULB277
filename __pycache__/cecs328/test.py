8e16029e997b41c2b224dc3e1c9b95793fbaae8a
nick noj <nickthe20@gmail.com>
2025-02-11 01:52:50 -0800

    removed 328 files

--- __pycache__/cecs328/test.py
+++
@@ -1,6 +0,0 @@
-from cecs328pa1 import asymptotic_analysis
-from sympy import symbols
-# Testing the function
-print(asymptotic_analysis("2**n", "3**n", "O"))  # Expected: True
-print(asymptotic_analysis("lg(n)", "n", "O"))  # Expected: True (lg is log base 2)
-print("hi")
