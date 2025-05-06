416a6485783d5431144e7c34f12fc4ba1be96752
Ariel W <38389139+Reljamin@users.noreply.github.com>
2025-05-06 18:47:07 +0000

    template for asleep state

--- lab 13/state_asleep.py
+++ lab 13/state_asleep.py
@@ -1,1 +1,15 @@
 import puppy_state
+import state_eat
+
+class StateAsleep(puppy_state.PuppyState):
+
+    def play(self, puppy):
+        
+
+        pass
+
+
+    def feed(self, puppy):
+
+
+        pass
